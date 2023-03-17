8dccb0e5362d52fa941812606789c91e77f0b8f0
Barahlush <baraltiva@gmail.com>
2023-03-17 13:36:28 +0300

    Adds page loader class

--- della_parser_bot/parser/parser.py
+++ della_parser_bot/parser/parser.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime, timedelta
 from urllib.parse import urljoin
 
+import requests
 from bs4.element import Tag
 
 SECONDS_IN_MINUTE, SECONDS_IN_HOUR = 60, 3600
@@ -172,3 +173,37 @@ class DellaParser:
             date,
             link,
         )
+
+
+class DellaDownloader:
+    """Downloads della.kz pages"""
+
+    def __init__(self) -> None:
+        self.base_url = 'https://www.della.kz/'
+
+    def load_page(self, url: str | None, path: str | None) -> str | None:
+        """Loads della.kz page
+
+        Args:
+            url (str | None): url of della.kz page, optional
+            path (str | None): path of della.kz page, for
+                example, /search/a98bdeflolh0i230301l230516k0m1.html
+
+        Raises:
+            ValueError: Page or path must be specified
+            ValueError: Only one of page or path must be specified
+
+        Returns:
+            str: html source of della.kz page
+        """
+        if not url and not path:
+            raise ValueError('Page or path must be specified')
+        if url and path:
+            raise ValueError('Only one of page or path must be specified')
+        if not url:
+            url = urljoin(self.base_url, path)
+
+        result = requests.get(url, timeout=10)
+        if result.ok:
+            return result.text
+        return None
